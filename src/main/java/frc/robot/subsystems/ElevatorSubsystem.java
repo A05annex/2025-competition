44891e904d27bd48806b0971c79e70cc14af23e1
ethanready <ethanready@icloud.com>
2025-01-24 16:28:50 -0800

    Added ElevatorSubsystem; PIDs and positions need tuning
    -Judah

---
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -0,0 +1,98 @@
+package frc.robot.subsystems;
+
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import org.a05annex.frc.subsystems.SparkNeo;
+import org.a05annex.util.Utl;
+
+public class ElevatorSubsystem extends SubsystemBase {
+
+    private final SparkNeo motor = SparkNeo.factory(Constants.CAN_Devices.SAMPLE_MOTOR);
+
+    // Declare PID constants for smart motion control
+    @SuppressWarnings("FieldCanBeLocal")
+	private final double smKp = 0.00005, smKi = 0.000, smKiZone = 0.0, smKff = 0.000156, smMaxRPM = 3000.0,
+            smMaxDeltaRPMSec = 3000.0, smError = 0.1;
+
+    // Declare PID constants for position control
+    @SuppressWarnings("FieldCanBeLocal")
+    private final double posKp = 0.22, posKi = 0.0, posKiZone = 0.0, posKff = 0.0;
+
+    // Declare PID constants for speed (rpm) control
+    @SuppressWarnings("FieldCanBeLocal")
+    private final double rpmKp = 0.5, rpmKi = 0.0, rpmKiZone = 0.0, rpmKff = 0.0;
+
+    // Declare min and max soft limits and where the motor thinks it starts
+    @SuppressWarnings("FieldCanBeLocal")
+    private final Double minPosition = 0.0, maxPosition = 1000.0, startPosition = 500.0;
+
+    //TODO: This is a placeholder value
+    private final double positionTolerence = 1;
+
+    private final static ElevatorSubsystem INSTANCE = new ElevatorSubsystem();
+    public static ElevatorSubsystem getInstance() {
+        return INSTANCE;
+    }
+
+    private ElevatorSubsystem() {
+        motor.startConfig();
+        motor.setCurrentLimit(SparkNeo.UseType.RPM_OCCASIONAL_STALL, SparkNeo.BreakerAmps.Amps40);
+		motor.setSoftLimits(minPosition, maxPosition);
+        motor.setDirection(SparkNeo.Direction.REVERSE);
+        //motor.setIdleMode(SparkBaseConfig.IdleMode.kBrake);
+        motor.setPositionPID(posKp, posKi, posKiZone, posKff);
+        motor.setMAXMotionPosition(smKp, smKi, smKiZone, smKff, smMaxRPM, smMaxDeltaRPMSec, smError);
+        motor.setRpmPID(rpmKp, rpmKi, rpmKiZone, rpmKff);
+        motor.endConfig();
+        motor.setEncoderPosition(startPosition);
+    }
+
+    @SuppressWarnings("unused")
+    public void goToMAXMotionPosition(double position) {
+        motor.setTargetMAXMotionPosition(position);
+    }
+
+    @SuppressWarnings("unused")
+    public void resetEncoder() {
+        motor.setEncoderPosition(0.0);
+    }
+
+    @SuppressWarnings("unused")
+    public void stop() {
+        motor.stopMotor();
+    }
+
+    public double getPosition() {
+        return motor.getEncoderPosition();
+    }
+
+    public boolean isInPosition(double position) {
+        return Utl.inTolerance(getPosition(), position, positionTolerence);
+    }
+
+    public enum ELEVATOR_POSITION {
+        AGI(0.0),
+        HPI(50.0),
+        L1(250.0),
+        L2(500.0),
+        L3(1000.0);
+
+        public final double position;
+
+        private static final ElevatorSubsystem elevatorSubsystem = ElevatorSubsystem.getInstance();
+
+        ELEVATOR_POSITION(double position) {
+            this.position = position;
+        }
+
+        public void goTo() {
+            elevatorSubsystem.goToMAXMotionPosition(position);
+        }
+
+        public boolean isInPosition() {
+            return elevatorSubsystem.isInPosition(position);
+        }
+    }
+}
+
