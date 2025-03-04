5063ed74ba035022b5501eb8a4f33cb96d3ba8a0
AssaultBurrow <carson.tokstad@outlook.com>
2025-03-03 16:17:27 -0800

    Refactored the AlgaeDislodgeCommand

--- src/main/java/frc/robot/commands/AlgaeDislodgeCommand.java
+++ src/main/java/frc/robot/commands/AlgaeDislodgeCommand.java
@@ -12,8 +12,6 @@ public class AlgaeDislodgeCommand extends Command {
 
     AlgaeSubsystem algaeSubsystem = AlgaeSubsystem.getInstance();
 
-    int fallWaitCounter = -1;
-
     ElevatorSubsystem.ELEVATOR_POSITION position;
 
     public AlgaeDislodgeCommand() {
@@ -24,30 +22,25 @@ public class AlgaeDislodgeCommand extends Command {
     public void initialize() {
         position = Constants.getDPad(Constants.ALT_XBOX) == A05Constants.D_PAD.D ?
                 ElevatorSubsystem.ELEVATOR_POSITION.ALGAE_LOW : ElevatorSubsystem.ELEVATOR_POSITION.ALGAE_HIGH;
-        elevatorSubsystem.goToMAXMotionPosition(position.position + elevatorSubsystem.inchesToEncoder(5.0));
+        elevatorSubsystem.goToMAXMotionPosition(position.position - elevatorSubsystem.inchesToEncoder(5.0));
         algaeSubsystem.setVelocity(3000.0);
-        fallWaitCounter = -1;
     }
 
     @Override
     public void execute() {
-        if(elevatorSubsystem.isInPosition(position.position + elevatorSubsystem.inchesToEncoder(5.0))) {
+        if(elevatorSubsystem.isInPosition(position.position - elevatorSubsystem.inchesToEncoder(5.0))) {
             position.goTo();
-            fallWaitCounter = 0;
         }
-
-        // Use tertiary operator to increment fallWaitCounter after it does not equal -1.
-        fallWaitCounter = fallWaitCounter != -1 ? fallWaitCounter + 1 : fallWaitCounter;
     }
 
     @Override
     public boolean isFinished() {
-        return fallWaitCounter > 25;
+        return position.isInPosition();
     }
 
     @Override
     public void end(boolean interrupted) {
         algaeSubsystem.stop();
-        ElevatorSubsystem.ELEVATOR_POSITION.ALGAE_HOLD.goTo();
+        ElevatorSubsystem.ELEVATOR_POSITION.SAFE.goTo();
     }
 }
