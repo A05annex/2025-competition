cb6b8e8f8bc803db36cc3dff567f4920deb05c29
ethanready <ethanready@icloud.com>
2025-03-12 16:15:11 -0700

    Refactor L1ScoreCommandGroup to use dynamic D-Pad input for CoralTroughScoreCommand

--- src/main/java/frc/robot/commands/L1ScoreCommandGroup.java
+++ src/main/java/frc/robot/commands/L1ScoreCommandGroup.java
@@ -3,11 +3,12 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants;
 import frc.robot.subsystems.ElevatorSubsystem;
 import org.a05annex.frc.A05Constants;
 
 public class L1ScoreCommandGroup extends SequentialCommandGroup {
     public L1ScoreCommandGroup() {
-        super(Commands.parallel(new ElevatorMoveWaitCommand(ElevatorSubsystem.ELEVATOR_POSITION.L1), new ReefTargetCommand(-0.5, A05Constants.D_PAD.D)), new CoralTroughScoreCommand());
+        super(Commands.parallel(new ElevatorMoveWaitCommand(ElevatorSubsystem.ELEVATOR_POSITION.L1), new ReefTargetCommand(-0.5, A05Constants.D_PAD.D)), new CoralTroughScoreCommand(Constants.getDPad(A05Constants.ALT_XBOX) == A05Constants.D_PAD.DR));
     }
 }
