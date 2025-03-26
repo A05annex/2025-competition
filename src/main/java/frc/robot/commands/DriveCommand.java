bc0ab8fc3331a86cd9535ec2aaf4bff40bea494d
ethanready <ethanready@icloud.com>
2025-03-25 23:44:10 -0700

    Refactor DriveCommand to improve heading source management and clean up state  logic

--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -4,7 +4,6 @@ import frc.robot.Constants;
 import frc.robot.RobotContainer;
 import org.a05annex.frc.A05Constants;
 import org.a05annex.frc.InferredRobotPosition;
-import org.a05annex.frc.RobotPosition;
 import org.a05annex.frc.commands.A05DriveCommand;
 import org.a05annex.frc.subsystems.ISwerveDrive;
 import org.a05annex.util.AngleD;
@@ -15,155 +14,166 @@ import org.a05annex.util.Utl;
  * Drive command is here because you will likely need to override the serve (targeting, competition specific reason)
  */
 public class DriveCommand extends A05DriveCommand {
-    private A05Constants.D_PAD direction = A05Constants.D_PAD.NONE;
-
-    private boolean isTargetingHeading = false;
-
+	private A05Constants.D_PAD direction = A05Constants.D_PAD.NONE;
 	private final String[] keyList = {"close center reef", "far center reef", "close left reef", "far left reef", "close right reef", "far right reef"};
-
 	private int driveTimeout;
-    private int altTimeout;
-
+	private int altTimeout;
 	private String lastKey = "";
+	private HeadingSource headingSource = HeadingSource.NONE;
 
-	private int tagSwitchTimeout;
+	private enum HeadingSource {
+		NONE,
+		XBOX,
+		APRILTAG
+	}
 
-    /**
-     * Default command for DriveSubsystem. Left stick moves the robot field-relatively, and right stick X rotates.
-     * Contains driver constants for sensitivity, gain, and deadband.
-     */
-    public DriveCommand(ISwerveDrive swerveDrive) {
-        super(swerveDrive);
-        // each subsystem used by the command must be passed into the
-        // addRequirements() method (which takes a vararg of Subsystem)
-    }
+	/**
+	 * Default command for DriveSubsystem. Left stick moves the robot field-relatively, and right stick X rotates.
+	 * Contains driver constants for sensitivity, gain, and deadband.
+	 */
+	public DriveCommand(ISwerveDrive swerveDrive) {
+		super(swerveDrive);
+		// each subsystem used by the command must be passed into the
+		// addRequirements() method (which takes a vararg of Subsystem)
+	}
 
-    @Override
-    public void initialize() {
-        direction = A05Constants.D_PAD.NONE;
-        isTargetingHeading = false;
-        altTimeout = 0;
+	@Override
+	public void initialize() {
+		resetState();
+	}
+
+	@Override
+	public void execute() {
+		double rotate = lastConditionedRotate;
+
+		conditionStick(); // Do the normal drive selection based on the drive sticks
+
+		chooseHeadingSource(); // Check if we have inputs which would use an alternate rotation algorithm
+
+		// If the right stick is not centered (driver is trying to spin), reset the state
+		if(!Utl.inTolerance(driveXbox.getRightX(), 0.0, 0.05)) {
+			resetState();
+		}
+
+		// If we have a heading source, calculate the rotation
+		if(headingSource != HeadingSource.NONE) {
+			lastConditionedRotate = rotate;
+			if(headingSource == HeadingSource.XBOX) {
+				calcXboxHeadingRotation();
+			} else if(headingSource == HeadingSource.APRILTAG) {
+				calcAprilTagHeadingRotation();
+			}
+		}
+		iSwerveDrive.swerveDrive(conditionedDirection, conditionedSpeed, conditionedRotate);
+	}
+
+	private void resetState() {
+		direction = A05Constants.D_PAD.NONE;
+		altTimeout = 0;
 		driveTimeout = 0;
-		tagSwitchTimeout = 0;
 		lastKey = "";
-    }
+		headingSource = HeadingSource.NONE;
+	}
 
-    @Override
-    public void execute() {
-        //This runs the default swerve calculations for xbox control
-        //super.execute();
+	private void chooseHeadingSource() {
+		if(RobotContainer.driveRightBumper.getAsBoolean()) {
+			headingSource = HeadingSource.APRILTAG;
+			return;
+		}
 
-        double rotate = lastConditionedRotate;
-
-        conditionStick();
-
-
-        for(A05Constants.D_PAD direction : A05Constants.D_PAD.values()) {
-			if(RobotContainer.driveRightBumper.getAsBoolean()) {
-				isTargetingHeading = true;
-				this.direction = A05Constants.D_PAD.NONE;
-				break;
-			}
-
-			if((driveTimeout > 0 && Constants.getDPad(driveXbox) == direction)
-					|| (altTimeout > 0 &&  RobotContainer.altLeftBumper.getAsBoolean() && Constants.getDPad(Constants.ALT_XBOX) == direction)
-					&& direction != this.direction) {
-				driveTimeout = driveTimeout > 0 ? driveTimeout - 1 : 0;
-				altTimeout = altTimeout > 0 ? altTimeout - 1 : 0;
-			}
-
+		for(A05Constants.D_PAD direction : A05Constants.D_PAD.values()) {
+			updateTimeouts(direction);
 			if(direction == A05Constants.D_PAD.NONE) {
 				continue;
 			}
-
 			if(Constants.getDPad(driveXbox) == direction && driveTimeout <= 0) {
 				driveTimeout = this.direction != A05Constants.D_PAD.NONE ? 3 : 0;
 				this.direction = direction;
-				isTargetingHeading = true;
-            } else if(RobotContainer.altLeftBumper.getAsBoolean() && Constants.getDPad(Constants.ALT_XBOX) == direction && altTimeout <= 0) {
+				headingSource = HeadingSource.XBOX;
+			} else if(RobotContainer.altLeftBumper.getAsBoolean() && Constants.getDPad(Constants.ALT_XBOX) == direction && altTimeout <= 0) {
 				altTimeout = this.direction != A05Constants.D_PAD.NONE ? altTimeout - 1 : 0;
 				this.direction = direction;
-				isTargetingHeading = true;
-            }
-        }
+				headingSource = HeadingSource.XBOX;
+			}
+		}
+	}
 
-        if(!Utl.inTolerance(driveXbox.getRightX(), 0.0, 0.05)) {
-            isTargetingHeading = false;
-			direction = A05Constants.D_PAD.NONE;
+	private void updateTimeouts(A05Constants.D_PAD direction) {
+		if((driveTimeout > 0 && Constants.getDPad(driveXbox) == direction)
+				|| (altTimeout > 0 && RobotContainer.altLeftBumper.getAsBoolean() && Constants.getDPad(Constants.ALT_XBOX) == direction)
+				&& direction != this.direction) {
+			driveTimeout = driveTimeout > 0 ? driveTimeout - 1 : 0;
+			altTimeout = altTimeout > 0 ? altTimeout - 1 : 0;
+		}
+	}
 
-			altTimeout = 0;
-			driveTimeout = 0;
-			tagSwitchTimeout = 0;
-        }
+	private void calcXboxHeadingRotation() {
+		double heading =
+				switch(direction) {
+					case U -> 0.0;
+					case UR -> 60.0;
+					case DR -> 120.0;
+					case D -> 180.0;
+					case DL -> 240.0;
+					case UL -> 300.0;
+					case R -> 90.0;
+					case L -> 270.0;
+					default -> -1.0;
+				};
 
-        if(isTargetingHeading) {
-            lastConditionedRotate = rotate;
-            calcTargetHeadingRotation();
-        }
+		if(heading == -1.0) {
+			return;
+		}
 
-        iSwerveDrive.swerveDrive(conditionedDirection, conditionedSpeed, conditionedRotate);
-    }
+		updateHeading(heading);
+	}
 
-    private void calcTargetHeadingRotation() {
-		double bestY = 1000.0;
+	private void calcAprilTagHeadingRotation() {
+		double bestAngle = 360.0;
 		String bestTagSetKey = "";
 
-		double heading = switch(direction) {
-			case U -> 0.0;
-			case UR -> 60.0;
-			case DR -> 120.0;
-			case D -> 180.0;
-			case DL -> 240.0;
-			case UL -> 300.0;
-            case R -> 90.0;
-            case L -> 270.0;
-			default -> -1.0;
-		};
 
+		// If the lastKey is set (not empty), set lowestAngle to its value (if possible). This will bias the algorithm
+		// towards the last tag set to remove oscillation
 		if(!lastKey.isEmpty()) {
 			InferredRobotPosition lastRobotPosition = InferredRobotPosition.getRobotPosition(Constants.aprilTagSetDictionary.get(lastKey));
 			if(lastRobotPosition.isValid) {
-				bestY = Math.abs(lastRobotPosition.y); // If the y value is less than the previous best y value, set the best y value to this y value
+				bestAngle = Math.abs(Math.atan(lastRobotPosition.y / lastRobotPosition.x));
 			}
 		}
 
-		if(direction == A05Constants.D_PAD.NONE) {
-			for(String key : keyList) {
-				InferredRobotPosition robotPosition = InferredRobotPosition.getRobotPosition(Constants.aprilTagSetDictionary.get(key));
-				if(robotPosition.isNew && !key.equals(lastKey)) {
-					System.out.println("New Reported Y: " + Math.abs(robotPosition.y) + "\nNew Key: " + key);
-					if(Math.abs(robotPosition.y) < bestY - 0.8) {
-						bestY = Math.abs(robotPosition.y);
-						bestTagSetKey = key; // If the current tag is now best, update the best tag set
-						lastKey = key;
-					}
-				}
+		// Iterate through the tag sets and find the one with the lowest angle
+		for(String key : keyList) {
+			InferredRobotPosition robotPosition = InferredRobotPosition.getRobotPosition(Constants.aprilTagSetDictionary.get(key)); // Get the position
 
-				bestTagSetKey = bestTagSetKey.isEmpty() ? lastKey : bestTagSetKey;
+			double newAngle = Math.abs(Math.atan(robotPosition.y / robotPosition.x));
 
-				if(bestY == 1000.0) {
-					lastKey = "";
-					return;
-				}
+			// The position needs to be new (the robot just saw the tag), it cannot be the lastKey (already checked).
+			// If the new angle is lower than the current best (lowest) angle by 10 degrees, update the lowest angle and best tag set key
+			if(robotPosition.isNew && !key.equals(lastKey) && newAngle < bestAngle - (10.0 * Math.PI / 180.0)) {
+				bestAngle = newAngle;
+				bestTagSetKey = key;
+				lastKey = key;
 			}
-			heading = Constants.aprilTagSetDictionary.get(bestTagSetKey).heading().getDegrees();
-		} else {
-			tagSwitchTimeout--;
 		}
 
-		 if(heading == -1.0) {
-			 return;
-		 }
+		bestTagSetKey = bestTagSetKey.isEmpty() ? lastKey : bestTagSetKey;
 
-		 if(tagSwitchTimeout <= 0 && !navX.getHeadingInfo().expectedHeading.equals(navX.getHeadingInfo().getClosestHeading(new AngleD(AngleUnit.DEGREES, heading)))) {
-         	 navX.setExpectedHeading(navX.getHeadingInfo().getClosestHeading(new AngleD(AngleUnit.DEGREES, heading)));
-			 tagSwitchTimeout = 0;
-		 } else {
-			 tagSwitchTimeout--;
-		 }
+		if(bestAngle == 360.0 || bestTagSetKey.isEmpty()) {
+			lastKey = "";
+			return;
+		}
 
-         conditionedRotate = new AngleD(navX.getHeadingInfo().expectedHeading).subtract(new AngleD(navX.getHeadingInfo().heading))
-                .getRadians() * A05Constants.getDriveOrientationKp();
-         conditionedRotate = Utl.clip(conditionedRotate, -0.5, 0.5);
+		updateHeading(Constants.aprilTagSetDictionary.get(bestTagSetKey).heading().getDegrees());
+	}
+
+	private void updateHeading(double heading) {
+		if(!navX.getHeadingInfo().expectedHeading.equals(navX.getHeadingInfo().getClosestHeading(new AngleD(AngleUnit.DEGREES, heading)))) {
+			navX.setExpectedHeading(navX.getHeadingInfo().getClosestHeading(new AngleD(AngleUnit.DEGREES, heading)));
+		}
+
+		conditionedRotate = new AngleD(navX.getHeadingInfo().expectedHeading).subtract(new AngleD(navX.getHeadingInfo().heading))
+				.getRadians() * A05Constants.getDriveOrientationKp();
+		conditionedRotate = Utl.clip(conditionedRotate, -0.5, 0.5);
 	}
 }
